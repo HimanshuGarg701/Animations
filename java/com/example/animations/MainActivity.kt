4bc49a8b24868e04c4cbb89ae35096d922f45572
Himanshu Garg <hgarg701@gmail.com>
2020-02-25 22:12:12 -0800

    Added more pics and specific functions

--- java/com/example/animations/MainActivity.kt
+++ java/com/example/animations/MainActivity.kt
@@ -4,12 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    fun clickFunction(view : View){
-        var mickey = findViewById<ImageView>(R.id.mickeyPic)
-        mickey.animate().translationXBy(1000f).setDuration(2000)
+    fun clickGoofyFunction(view : View){
+        val mickey = findViewById<ImageView>(R.id.goofyPic)
+        mickey.animate().translationXBy(2000f).setDuration(3000)
+        Toast.makeText(this,"Mickey Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickJerryFunction(view : View){
+        val jerry = findViewById<ImageView>(R.id.jerryPic)
+        jerry.animate().rotationBy(360f).setDuration(2000)
+        Toast.makeText(this,"Jerry Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickDogFunction(view : View){
+        val dog = findViewById<ImageView>(R.id.dogPic)
+        dog.animate().alpha(0.5f)
+        Toast.makeText(this,"Dog Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickPandaFunction(view : View){
+        val panda = findViewById<ImageView>(R.id.pandaPic)
+        panda.animate().translationYBy(2000f).setDuration(2000)
+        Toast.makeText(this,"Panda Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickSunFunction(view : View){
+        val sun = findViewById<ImageView>(R.id.sunPic)
+        sun.animate().alpha(0f).setDuration(2000)
+        Toast.makeText(this,"Sun Clicked", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
